678222f5d11552674a80d1972c311bddaa0806ce
wakenedo <a_nuner@outlook.com>
2022-04-27 17:35:50 -0300

    Swiper - Challenge Part 2
    Started to implement the first setup for the ContinentSlider Component

--- src/components/ContinentSlider/index.tsx
+++ src/components/ContinentSlider/index.tsx
@@ -1,20 +1,110 @@
 import { Box, Container, Image } from '@chakra-ui/react'
+import Swiper, { Navigation, Pagination } from 'swiper'
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+
+
+const swiper = new Swiper('.swiper', {
+    modules: [Navigation, Pagination],
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        bulletActiveClass: 'swiper-pagination-active',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',   
+    },
+});
 
 export function ContinentSlider() {
     return (
-        <Box>
+        <>
+            <div className="swiper">
+
+                <div className="swiper-wrapper">
+
+                    <div className="swiper-slide">
+                        <Image
+                            width={1440}
+                            height={460}
+                            marginBottom={10}
+                            src="images/Europe.png" alt="ContinentsSwipingBanner"
+                        />
+                    </div>
+
+                    <div className="swiper-slide">
+                        <Image
+                            width={1440}
+                            height={460}
+                            marginBottom={10}
+                            src="images/Asia.jpg" alt="ContinentsSwipingBanner"
+                        />
+                    </div>
+
+                    <div className="swiper-slide">
+                        <Image
+                            width={1440}
+                            height={460}
+                            marginBottom={10}
+                            src="images/Europe.png" alt="ContinentsSwipingBanner"
+                        />
+                    </div>
+
+                    <div className="swiper-slide">
+                        <Image
+                            width={1440}
+                            height={460}
+                            marginBottom={10}
+                            src="images/Europe.png" alt="ContinentsSwipingBanner"
+                        />
+                    </div>
+
+                    <div className="swiper-slide">
+                        <Image
+                            width={1440}
+                            height={460}
+                            marginBottom={10}
+                            src="images/Europe.png" alt="ContinentsSwipingBanner"
+                        />
+                    </div>
+
+
+                </div>
+
+                <div className="swiper-pagination"></div>
+
+
+                <div className="swiper-button-prev"></div>
+                <div className="swiper-button-next"></div>
+
+
+                <div className="swiper-scrollbar"></div>
+            </div>
+        </>
+    )
+}
+
+{/*<Box >        
             <Container
                 maxWidth={1440}
                 margin='auto'
+
             >
                 <Image
                     width={1440}
                     height={460}
                     marginBottom={10}
-                    
                     src="images/Continents.png" alt="ContinentsSwipingBanner"
                 />
             </Container>
-        </Box>
-    )
-}
+    </Box>*/}
